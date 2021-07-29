b741931dd4c25a0921a1724dd8cbb2aa71e30d07
lolouhlorraine <caslo.1515@gmail.com>
2021-07-29 09:43:49 +0100

    added app error handler

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask
+from flask.templating import render_template
 from controller.route import converter
 
-# register blueprints from controller folder
+def error_handler(e):
+    return render_template("index.html")
+
 app = Flask(__name__)
 app.register_blueprint(converter)
+app.register_error_handler(500, error_handler)
